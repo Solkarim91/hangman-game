7b53b79bc36b85ee3199e72a6609c55391a95700
Solomon Karim <solomon.karim@atos.net>
2025-06-11 14:48:35 +0100

    Move components to components/ui

---
+++ ui/src/app/constants.ts
@@ -0,0 +1,1 @@
+export const CATEGORY_TITLES = ['Countries', 'Phrases', 'Movies']
--- ui/src/app/page.tsx
+++ ui/src/app/page.tsx
@@ -1,15 +1,22 @@
-import { Logo } from "../components/logo";
+import { Card } from "@/components/ui/card";
+import { Logo } from "@/components/ui/logo";
+import { CATEGORY_TITLES } from "./constants";
 
 export default function Home() {
   return (
     <div className="mt-20 flex flex-col items-center gap-8">
       <Logo />
-      {/* TODO: replace below div with custom Card component */}
-      <div>
+      <Card>
         <p className="font-main">
           {"To get started, pick a category below and then hit 'PLAY'!"}
         </p>
-      </div>
+
+        <ul className="carousel">
+          {CATEGORY_TITLES.map((title) => (
+            <li key={title}>{title}</li>
+          ))}
+        </ul>
+      </Card>
     </div>
   );
 }
