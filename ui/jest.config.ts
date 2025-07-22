5b5ff8f55e978b87ebeb3d49e4edd519bf14f49e
Solomon Karim <solomon.karim@atos.net>
2025-07-22 11:19:01 +0100

    Update jest config

--- ui/jest.config.ts
+++ ui/jest.config.ts
@@ -2,12 +2,16 @@ import type { Config } from 'jest';
 
 const config: Config = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
   transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest',
+    "^.+\\.(ts|tsx)$": ["ts-jest", { tsconfig: "tsconfig.jest.json" }]
   },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  }
 };
 
 export default config;
