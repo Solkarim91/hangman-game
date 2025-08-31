f8cbd827024b8dfcf19b719c7b1c0a1905b91062
Sol Karim <solkarimlive@hotmail.co.uk>
2025-08-31 13:49:41 +0100

    Fix back button

--- ui/src/components/nav/back-button.tsx
+++ ui/src/components/nav/back-button.tsx
@@ -4,7 +4,7 @@ import { useRouter } from "next/navigation";
 
 export const BackButton = () => {
   const router = useRouter();
-  const handleBackButtonClick = () => router.back();
+  const handleBackButtonClick = () => router.push("/");
 
   return (
     <button className="z-1 cursor-pointer hover:opacity-85 mx-2" onClick={handleBackButtonClick}>
